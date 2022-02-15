90a722fa610f03111c4739aa7ef7bf9abd5edd68
Greg DiCristofaro <gregd@basistech.com>
2022-02-15 08:06:22 -0500

    bug fix

--- Core/src/org/sleuthkit/autopsy/discovery/ui/GroupListPanel.java
+++ Core/src/org/sleuthkit/autopsy/discovery/ui/GroupListPanel.java
@@ -79,8 +79,8 @@ final class GroupListPanel extends javax.swing.JPanel {
         + "  -The Hash Lookup module must be run on each data source if you want to filter by past occurrence.\n"
         + "GroupsListPanel.noDomainResults.message.text=No domains were found for the selected filters.\n\n"
         + "Reminder:\n"
-        + "  -The Central Repository module must be run on each data source if you want to filter or sort by past occurrences.\n"
-        + "GroupsListPanel.noResults.title.text=No results found"})
+        + "  -The Central Repository module must be run on each data source if you want to filter or sort by past occurrences",
+        "GroupsListPanel.noResults.title.text=No results found"})
     /**
      * Subscribe to and update list of groups in response to
      * SearchCompleteEvents.
