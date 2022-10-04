7fdec4bba4d870162d41a8e6f875dcfa1d3938c3
Greg DiCristofaro <gregd@basistech.com>
2022-10-03 20:08:26 -0400

    changes

--- unix/live_iso.sh
+++ unix/live_iso.sh
@@ -5,24 +5,46 @@
 
 set -e
 
-#installing java8
+echo "Turning on all repositories for apt..."
+sed -Ei 's/^# deb-src /deb-src /' /etc/apt/sources.list
+if [[ $? -ne 0 ]]; then
+    echo "Failed to turn on all repositories" >>/dev/stderr
+    exit 1
+fi
 
-apt-get purge ubiquity
-add-apt-repository ppa:webupd8team/java;
-apt-get update;
-apt-get install oracle-java8-installer;
-source /etc/profile.d/jdk.sh
+echo "Installing all apt dependencies..."
+apt update && \
+    apt -y install build-essential autoconf libtool automake git zip wget ant \
+        libde265-dev libheif-dev \
+        libpq-dev \
+        testdisk libafflib-dev libewf-dev libvhdi-dev libvmdk-dev \
+        libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad \
+        gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x \
+        gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
 
-#installing sleuthkit dependencies
+if [[ $? -ne 0 ]]; then
+    echo "Failed to install necessary dependencies" >>/dev/stderr
+    exit 1
+fi
 
-apt-get update && apt-get upgrade;
-apt-get install libtool automake libpq-dev postgresql libewf-dev libafflib-dev libvhdi-dev libvmdk-dev git testdisk ant build-essential aptitude wget unzip;
-apt-get install libstdc++6;
+echo "Installing bellsoft Java 8..."
+pushd /usr/src/ &&
+    wget -q -O - https://download.bell-sw.com/pki/GPG-KEY-bellsoft | sudo apt-key add - &&
+    echo "deb [arch=amd64] https://apt.bell-sw.com/ stable main" | sudo tee /etc/apt/sources.list.d/bellsoft.list &&
+    apt update &&
+    apt -y install bellsoft-java8-full &&
+    popd
+if [[ $? -ne 0 ]]; then
+    echo "Failed to install bellsoft java 8" >>/dev/stderr
+    exit 1
+fi
 
 #installing sleuthkit using the debian files
-
 if [ "$1" != ""  ]; then
-[ ! -f "./$1" ] && wget https://github.com/sleuthkit/sleuthkit/releases/download/sleuthkit-${1:15:5}/$1;
+TSK_REGEX="\\s*sleuthkit-java_([0-9\\.]*)"
+[[ $1 =~ $TSK_REGEX ]]
+TSK_VERSION_NUM=${BASH_REMATCH[1]}
+[ ! -f "./$1" ] && wget https://github.com/sleuthkit/sleuthkit/releases/download/sleuthkit-$TSK_VERSION_NUM/$1;
   apt-get -y install ./$1;
   rm ./$1
 fi
@@ -31,7 +53,7 @@ fi
 installAutopsy () {
 [ ! -f "./$1" ] && wget https://github.com/sleuthkit/autopsy/releases/download/${1%.*}/$1;
    [ ! -d "/${1%.*}" ] && unzip ./$1 -d /
-   cd /${1%.*} && sh unix_setup.sh && cd -
+   export JAVA_HOME=/usr/lib/jvm/bellsoft-java8-full-amd64 && cd /${1%.*} && sh unix_setup.sh && cd -
    rm ./$1 > /dev/null
  }
 
@@ -58,7 +80,7 @@ chmod +x /usr/share/applications/autopsy.desktop
 #setup desktop files
 mkdir /etc/skel/Desktop
 cp /usr/share/applications/autopsy.desktop /etc/skel/Desktop/
-cp /usr/share/applications/lxterminal.desktop /etc/skel/Desktop/
+cp /usr/share/applications/xfce4-terminal.desktop /etc/skel/Desktop/
 
 #setup autopsy icon
 [ ! -f "./autopsy.png" ] && wget https://github.com/sleuthkit/autopsy/raw/develop/unix/autopsy.png
@@ -66,8 +88,6 @@ mv ./autopsy.png /usr/share/icons
 
 #setup iso wallpaper
 [ ! -f "./autopsy_wallpaper1.png" ] && wget https://github.com/sleuthkit/autopsy/raw/develop/unix/autopsy_wallpaper1.png
-mv ./autopsy_wallpaper1.png /usr/share/lubuntu/wallpapers/autopsy_wallpaper.png
-unlink /usr/share/lubuntu/wallpapers/lubuntu-default-wallpaper.png
-unlink /usr/share/lubuntu/wallpapers/lubuntu-default-wallpaper.jpg
-ln -s /usr/share/lubuntu/wallpapers/autopsy_wallpaper.png /usr/share/lubuntu/wallpapers/lubuntu-default-wallpaper.png
-ln -s /usr/share/lubuntu/wallpapers/lubuntu-default-wallpaper.png /usr/share/lubuntu/wallpapers/lubuntu-default-wallpaper.jpg
+mv ./autopsy_wallpaper1.png /usr/share/xfce4/backdrops/autopsy_wallpaper.png
+unlink /usr/share/xfce4/backdrops/xubuntu-wallpaper.png
+ln -s /usr/share/xfce4/backdrops/autopsy_wallpaper.png /usr/share/xfce4/backdrops/xubuntu-wallpaper.png
